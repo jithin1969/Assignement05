61f8fd76ee39a3b83d4b595a0dc422efcbc704fa
jithin1969 <jithinbiju597@gmail.com>
2022-11-07 17:02:18 -0500

    initial commit

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 /*********************************************************************************
- *  WEB322 – Assignment 03
+ *  WEB322 – Assignment 04
  *  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part *  of this assignment has been copied manually or electronically from any other source
  *  (including 3rd party web sites) or distributed to other students.
  *
- *  Name:JithinBiju Student ID:153532213 Date: 16 october,2022
+ *  Name:JithinBiju Student ID:153532213 Date: 11-NOV-2022
  *
  *  Online (Cyclic) Link: 
  *
@@ -15,6 +15,8 @@
  const fileUpload = multer();
  const cloudinary = require('cloudinary').v2
  const streamifier = require('streamifier')
+ const exphbs = require('express-handlebars');
+ const stripJs = require('strip-js');
 
  cloudinary.config({
   cloud_name: 'dexdo1qrk',
@@ -22,6 +24,33 @@
   api_secret: 'gFHkoiGEm8gZ5kv0R9YACWalRYs',
       secure: true
   });
+
+  app.engine('.hbs', exphbs.engine({ 
+    extname: ".hbs", 
+    defaultLayout: "main",
+    helpers: {
+        navLink: function(url, options){
+            return '<li' + 
+                ((url == app.locals.activeRoute) ? ' class="active" ' : '') + '><a href="' + url + '">' + options.fn(this) + '</a></li>'; },
+        equal: function (lvalue, rvalue, options) {
+            if (arguments.length < 3)
+                throw new Error("Handlebars Helper equal needs 2 parameters");
+            if (lvalue != rvalue) {
+                return options.inverse(this);
+            } else {
+                return options.fn(this);
+            }
+        }           
+    } 
+}));
+
+app.use(function(req,res,next) {
+  let route = req.baseUrl+req.path;
+  app.locals.activeRoute = (route == "/") ? "/":route.replace(/\/$/,"");
+  next();
+});
+
+app.set('view engine', '.hbs');
 
   //adding path tp product-service.js module to interact with it
  var data = require("./product-service");
@@ -45,12 +74,14 @@
  
  //setting up a defualt route for local host
  
- app.get("/", function (req, res) {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
+ //home
+app.get('/', (req, res) => {
+  res.render(path.join(__dirname + "/views/home.hbs"));
 });
 
-app.get("/home", function (req, res) {
-  res.sendFile(path.join(__dirname + "/views/index.html"));
+//otherwise /home would return an error
+app.get('/home', (req, res) => {
+  res.render(path.join(__dirname + "/views/home.hbs"));
 });
 
 //route to products
@@ -70,24 +101,24 @@ app.get("/home", function (req, res) {
  app.get("/demos", (req, res) => {
   if (req.query.category) {
       data.getProductsByCategory(req.query.category).then((data) => {
-          res.json({data});
+        res.render("demos", {demos: data});
       }).catch((err) => {
-          res.json({message: err});
+        res.render("demos", {message: "no results"});
       })
   }
   else if (req.query.minDateStr) {
       data.getProductsByMinDate(req.query.minDateStr).then((data) => {
-          res.json({data});
+        res.render("demos", {demos: data});
       }).catch((err) => {
-          res.json({message: err});
+        res.render("demos", {message: "no results"});;
       })
   }
   
   else {
       data.getAllProducts().then((data) => {
-          res.json({data});
+        res.render("demos", {demos: data});
       }).catch((err) => {
-          res.json({message: err});
+        res.render("demos", {message: "no results"});
       })
   }
 });
@@ -97,10 +128,10 @@ app.get("/home", function (req, res) {
    data
      .getCategories()
      .then(function (data) {
-       res.json(data);
+      res.render("categories", {categories: data});
      })
      .catch(function (err) {
-       res.json({ message: err });
+      res.render("categories", {message: "no results"});
      });
  });
 
@@ -113,8 +144,8 @@ app.get("/home", function (req, res) {
   })
  });
 
- app.get("/products/add", (req, res) => {
-  res.sendFile(path.join(__dirname + "/views/addProducts.html"));
+ app.get('/products/add', (req, res) => {
+  res.render(path.join(__dirname + "/views/addproducts.hbs"));
 });
 app.post('/upload', fileUpload.single('image'), function (req, res, next) {
     let streamUpload = (req) => {
